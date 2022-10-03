b6449b59879f632f5bdfc6f75227aecfb2beadd2
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-03 21:47:40 +0100

    feat(Submit.tsx): Submit page -  where a user can submit a new post, - child of community Pages - NewPostForm added to the page where the user can add new posts,upload images etcetera

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,8 +1,20 @@
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import PageContent from '../../../components/Layout/PageContent';
+import NewPostForm from '../../../components/Posts/NewPostForm';
+import { auth } from '../../../firebase/clientApp';
 
-type submitProps = {};
+const SubmitPostPage: React.FC = () => {
+  const [user] = useAuthState(auth);
 
-const submit: React.FC<submitProps> = () => {
-  return <div>Have a good coding</div>;
+  return (
+    <PageContent>
+      {user && <NewPostForm user={user} />}
+      <Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
+        <Text fontWeight={600}>Create a post</Text>
+      </Box>
+    </PageContent>
+  );
 };
-export default submit;
+export default SubmitPostPage;
