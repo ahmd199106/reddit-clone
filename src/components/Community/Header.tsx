45121249e160678d0403458751f9a7dbf4cd71c4
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-06 17:43:26 +0100

    feat(header.tsx): use current community imageurl from communityStateValue instead of community Data passed from community index page via server side props to render the logog of a community in the community page header

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -16,60 +16,60 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
   const isJoined = !!communityStateValue.mySnippets.find(
     (item) => item.communityId === communityData.id,
   );
-
-  return (
-    <Flex direction='column' width='100%' height='146px'>
-      <Box height='50%' bg='blue.400' />
-      <Flex justifyContent='center' bg='white' height='50%'>
-        <Flex width='95%' maxWidth='860px'>
-          {/* IMAGE URL IS ADDED AT THE VERY END BEFORE DUMMY DATA - USE ICON AT FIRST */}
-          {communityData.imageURL ? (
-            <Image
-              borderRadius='full'
-              boxSize='66px'
-              src={communityData.imageURL}
-              alt='Dan Abramov'
-              position='relative'
-              top={-3}
-              color='blue.500'
-              border='4px solid white'
-            />
-          ) : (
-            <Icon
-              as={FaReddit}
-              fontSize={64}
-              position='relative'
-              top={-3}
-              color='blue.500'
-              border='4px solid white'
-              borderRadius='50%'
-            />
-          )}
-          <Flex padding='10px 16px'>
-            <Flex direction='column' mr={6}>
-              <Text fontWeight={800} fontSize='16pt'>
-                {communityData.id}
-              </Text>
-              <Text fontWeight={600} fontSize='10pt' color='gray.400'>
-                r/{communityData.id}
-              </Text>
-            </Flex>
-            <Flex>
-              <Button
-                variant={isJoined ? 'outline' : 'solid'}
-                height='30px'
-                pr={6}
-                pl={6}
-                onClick={() => onJoinLeaveCommunity(communityData, isJoined)}
-                isLoading={loading}
-              >
-                {isJoined ? 'Joined' : 'Join'}
-              </Button>
-            </Flex>
+console.log(communityData.creatorId, 'creator id');
+return (
+  <Flex direction='column' width='100%' height='146px'>
+    <Box height='50%' bg='blue.400' />
+    <Flex justifyContent='center' bg='white' height='50%'>
+      <Flex width='95%' maxWidth='860px'>
+        {/* IMAGE URL IS ADDED AT THE VERY END BEFORE DUMMY DATA - USE ICON AT FIRST */}
+        {communityStateValue.currentCommunity?.imageURL ? (
+          <Image
+            borderRadius='full'
+            boxSize='66px'
+            src={communityStateValue.currentCommunity.imageURL}
+            alt='Dan Abramov'
+            position='relative'
+            top={-3}
+            color='blue.500'
+            border='4px solid white'
+          />
+        ) : (
+          <Icon
+            as={FaReddit}
+            fontSize={64}
+            position='relative'
+            top={-3}
+            color='blue.500'
+            border='4px solid white'
+            borderRadius='50%'
+          />
+        )}
+        <Flex padding='10px 16px'>
+          <Flex direction='column' mr={6}>
+            <Text fontWeight={800} fontSize='16pt'>
+              {communityData.id}
+            </Text>
+            <Text fontWeight={600} fontSize='10pt' color='gray.400'>
+              r/{communityData.id}
+            </Text>
+          </Flex>
+          <Flex>
+            <Button
+              variant={isJoined ? 'outline' : 'solid'}
+              height='30px'
+              pr={6}
+              pl={6}
+              onClick={() => onJoinLeaveCommunity(communityData, isJoined)}
+              isLoading={loading}
+            >
+              {isJoined ? 'Joined' : 'Join'}
+            </Button>
           </Flex>
         </Flex>
       </Flex>
     </Flex>
-  );
+  </Flex>
+);
 };
 export default Header;
