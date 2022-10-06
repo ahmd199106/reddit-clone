ce3d4a32926b2504e1029e56dba94bfe4a9120e1
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-06 17:41:10 +0100

    feat(Post Page): extract the file select and ulpoad for uploaoding images while creating a new post into anew hook called useSelectFile to reuse in NewPostForm

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -13,6 +13,7 @@ import { BiPoll } from 'react-icons/bi';
 import { BsLink45Deg, BsMic } from 'react-icons/bs';
 import { IoDocumentText, IoImageOutline } from 'react-icons/io5';
 import { firestore, storage } from '../../firebase/clientApp';
+import useSelectFile from '../../hooks/useSelectFile';
 import ImageUpload from './PostsForm/ImageUpload';
 import TextInputs from './PostsForm/TextInputs';
 import TabItem from './TabItem';
@@ -55,11 +56,12 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
     title: '',
     body: '',
   });
-  const [selectedFile, setSelectedFile] = useState<string>();
+  // const [selectedFile, setSelectedFile] = useState<string>();
   const selectFileRef = useRef<HTMLInputElement>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const router = useRouter();
+  const { selectedFile, setSelectedFile, onSelectFile } = useSelectFile();
 
   const handleCreatePost = async () => {
     const { communityId } = router.query;
@@ -100,18 +102,18 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
     setLoading(false);
   };
 
-  const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-    if (event.target.files?.[0]) {
-      reader.readAsDataURL(event.target.files[0]);
-    }
+  // const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const reader = new FileReader();
+  //   if (event.target.files?.[0]) {
+  //     reader.readAsDataURL(event.target.files[0]);
+  //   }
 
-    reader.onload = (readerEvent) => {
-      if (readerEvent.target?.result) {
-        setSelectedFile(readerEvent.target?.result as string);
-      }
-    };
-  };
+  //   reader.onload = (readerEvent) => {
+  //     if (readerEvent.target?.result) {
+  //       setSelectedFile(readerEvent.target?.result as string);
+  //     }
+  //   };
+  // };
 
   const onTextChange = ({
     target: { name, value },
@@ -149,7 +151,7 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
             setSelectedFile={setSelectedFile}
             setSelectedTab={setSelectedTab}
             selectFileRef={selectFileRef}
-            onSelectImage={onSelectImage}
+            onSelectImage={onSelectFile}
           />
         )}
       </Flex>
@@ -161,6 +163,6 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
       )}
     </Flex>
   );
-};
+};;;
 
 export default NewPostForm;
