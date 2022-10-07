786d74fc60abefdfeb57ee589372c600260f300c
MohammedAK1991 <ahmd199106@gmail.com>
2022-10-07 20:37:33 +0100

    feat(submit.tsx): add the About component to the Submit page (submit a new post page)

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -3,22 +3,33 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRecoilValue } from 'recoil';
 import { communityState } from '../../../atoms/communitiesAtom';
+import About from '../../../components/Community/About';
 import PageContent from '../../../components/Layout/PageContent';
 import NewPostForm from '../../../components/Posts/NewPostForm';
 import { auth } from '../../../firebase/clientApp';
+import useCommunityData from '../../../hooks/useCommunityData';
 
 const SubmitPostPage: React.FC = () => {
   const [user] = useAuthState(auth);
-const communityStateValue = useRecoilValue(communityState);
-console.log(communityStateValue, 'COMMUNITY');
-
+  const { communityStateValue } = useCommunityData();
+  console.log(communityStateValue, 'COMMUNITY');
 
   return (
     <PageContent>
-      {user && <NewPostForm user={user} />}
-      <Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
-        <Text fontWeight={600}>Create a post</Text>
-      </Box>
+      <>
+        <Box p='14px 0px' borderBottom='1px solid' borderColor='white'>
+          <Text fontWeight={600}>Create a post</Text>
+        </Box>
+
+        {user && <NewPostForm user={user} />}
+      </>
+      <>
+        {communityStateValue.currentCommunity && (
+          <>
+            <About communityData={communityStateValue.currentCommunity} />
+          </>
+        )}
+      </>
     </PageContent>
   );
 };
